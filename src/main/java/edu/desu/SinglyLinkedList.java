a82daeb916108049a97dbc2cd0ad6e345e1e14ce
justint <justintolbertpro@gmail.com>
2023-11-01 16:25:56 -0400

    Finished code and tests.

--- src/main/java/edu/desu/SinglyLinkedList.java
+++ src/main/java/edu/desu/SinglyLinkedList.java
@@ -4,7 +4,72 @@ public class SinglyLinkedList<T> {
 
     private Node<T> headNode;
 
-    public SinglyLinkedList(){
+    public SinglyLinkedList() {
+    }
+
+    public boolean isEmpty() {
+        return headNode == null;
+    }
+
+    public boolean delete(T data) {
+        if (headNode == null){
+            return false;
+        }
+
+        Node<T> node = new Node<>(data);
+        Node<T> mainNode = headNode;
+        while (mainNode.getNextNode() != null) {
+            if (mainNode.getNextNode() == node) {
+                mainNode.setNextNode(mainNode.getNextNode());
+                return true;
+            }
+            mainNode = mainNode.getNextNode();
+        }
+        return false;
+    }
+
+    public boolean deleteAtEnd() {
+        if (headNode == null){
+            return false;
+        }
+
+        if (headNode.getNextNode() == null) {
+            headNode = null;
+            return true;
+        }
+
+        Node<T> node = headNode;
+        while (node.getNextNode() != null) {
+            node = node.getNextNode();
+        }
+        node.setNextNode(null);
+        return true;
+    }
+
+    public boolean deleteAtHead() {
+        if (headNode == null) {
+            return false;
+        }
+
+        headNode = headNode.getNextNode();
+        return true;
+    }
+
+    public boolean insertAfter(T previous, T data) {
+        Node<T> newNode = new Node<>(data);
+        Node<T> lastNode = new Node<>(previous);
+        Node<T> mainNode = headNode;
+        while (mainNode != null) {
+            if (mainNode.getData() == previous) {
+                newNode.setNextNode(mainNode.getNextNode());
+                lastNode.setNextNode(newNode);
+                this.headNode = lastNode;
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
     }
 
     public void insertAtHead(T data){
@@ -12,6 +77,30 @@ public class SinglyLinkedList<T> {
         Node<T> node = new Node<>(data);
         node.setNextNode(previousNode);
         this.headNode = node;
+    }
+
+    public void insertAtEnd(T data){
+        if(headNode == null){
+            insertAtHead(data);
+            return;
+        }
+        Node<T> currentNode = headNode;
+        while(currentNode.hasNext()){
+            Node<T> nextNode = currentNode.getNextNode();
+            currentNode = nextNode;
+        }
+        currentNode.setNextNode(new Node<>(data));
+    }
+
+    public boolean contains(T data) {
+        Node<T> node = headNode;
+        while (node != null){
+            if (node.getData() == data) {
+                return true;
+            }
+            node = node.getNextNode();
+        }
+        return false;
     }
 
     public String toString(){
--- src/test/java/edu/desu/SinglyLinkedListTest.java
+++ src/test/java/edu/desu/SinglyLinkedListTest.java
@@ -17,6 +17,37 @@ public class SinglyLinkedListTest {
     }
 
     @Test
+    public void isEmptyTest01(){
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<>();
+        Boolean actual = linkedList.isEmpty();
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    public void isEmptyTest02(){
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<>();
+        linkedList.insertAtHead("Test");
+        Boolean actual = linkedList.isEmpty();
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    public void insertAfterTest01(){
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<>();
+        linkedList.insertAtHead("First");
+        Boolean actual = linkedList.insertAfter("First", "Second");
+        System.out.println(linkedList.toString());
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    public void insertAfterTest02(){
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<>();
+        Boolean actual = linkedList.insertAfter("First", "Second");
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
     public void insertAtHeadTest02(){
         SinglyLinkedList<String> linkedList = new SinglyLinkedList<>();
         linkedList.insertAtHead("Test 01");
@@ -26,4 +57,144 @@ public class SinglyLinkedListTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void insertAtEndTest01(){
+        // given
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<>();
+        linkedList.insertAtHead("Test 01");
+        linkedList.insertAtHead("Test 00");
+        linkedList.insertAtEnd("Test 02");
+        // when
+
+        String expected = "Test 00,Test 01,Test 02";
+        String actual = linkedList.toString();
+        // then
+        System.out.println(actual);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void insertAtEndTest02(){
+        // given
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<>();
+        linkedList.insertAtEnd("Test 02");
+        // when
+
+        String expected = "Test 02";
+        String actual = linkedList.toString();
+        // then
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void deleteTest01(){
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<>();
+        linkedList.insertAtHead("Test 01");
+        linkedList.insertAtHead("Test 00");
+        linkedList.insertAtEnd("Test 02");
+        linkedList.delete("Test 01");
+
+        //String expected = "Test 00,Test02";
+        boolean actual = linkedList.delete("Test 01");
+
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    public void deleteTest02(){
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<>();
+
+        //String expected = "";
+        boolean actual = linkedList.delete("Test 5r");
+
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    public void deleteAtEndTest01(){
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<>();
+        linkedList.insertAtHead("Test 01");
+        linkedList.insertAtHead("Test 00");
+        linkedList.insertAtEnd("Test 02");
+        linkedList.deleteAtEnd();
+
+        boolean actual = linkedList.deleteAtEnd();
+
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    public void deleteAtEndTest02(){
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<>();
+        linkedList.deleteAtEnd();
+
+        boolean actual = linkedList.deleteAtEnd();
+
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    public void deleteAtHead01(){
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<>();
+        linkedList.insertAtHead("123");
+        linkedList.insertAtHead("456");
+        linkedList.insertAtHead("789");
+        linkedList.deleteAtHead();
+
+        boolean actual = linkedList.deleteAtEnd();
+
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    public void deleteAtHead02(){
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<>();
+        boolean actual = linkedList.deleteAtHead();
+
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    public void containsTest01(){
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<>();
+        linkedList.insertAtHead("Test 01");
+        linkedList.insertAtHead("Test 00");
+        linkedList.insertAtEnd("Test 02");
+        linkedList.contains("Test 01");
+
+        boolean actual = linkedList.contains("Test 02");
+
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    public void containsTest02(){
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<>();
+        linkedList.insertAtHead("Test 01");
+        linkedList.insertAtHead("Test 00");
+        linkedList.insertAtEnd("Test 02");
+        linkedList.contains("Winner 01");
+
+        boolean actual = linkedList.contains("Winner 02");
+
+        Assertions.assertFalse(actual);
+    }
 }
+
+    /*
+    @Test
+    public void insertAfterTest02(){
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<>();
+        linkedList.insertAtHead("Test 01");
+        linkedList.insertAtHead("Test 02");
+        linkedList.insertAfter("Test 02", "MiddleTest");
+        linkedList.insertAfter("MiddleTest", "AfterMiddleTest");
+        String expected = "Test 02,MiddleTest,AfterMiddleTest,Test 01";
+        String actual = linkedList.toString();
+        System.out.println(linkedList);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+     */
